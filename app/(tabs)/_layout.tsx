ac9fd40550b4e653d23e2aec766f70df1603d9f1
BrendonBernardino <brendonrsb@hotmail.com>
2024-05-18 18:05:54 -0300

    first

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,50 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+
+import * as Animatable from 'react-native-animatable';
+
+import IconCampo from 'react-native-vector-icons/MaterialCommunityIcons';
+import IconDado from 'react-native-vector-icons/Ionicons';
+// import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+// import CampoIcon from '../../assets/icons/soccer.svg';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        // tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // tabBarActiveBackgroundColor: '#1A291A',
+        headerShown: false,
+        tabBarShowLabel: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+              <IconCampo name="soccer-field" 
+              size={40}
+              color={focused ? '#4E9F3D' : color}
+              />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="sorteio"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+              <IconDado name="dice" 
+                size={40}
+                color={focused ? '#4E9F3D' : color}
+              />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
